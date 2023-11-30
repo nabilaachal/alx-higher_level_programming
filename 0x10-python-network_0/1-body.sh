550e8c3dc579f6132cf01fe533cfc6d14f4e14d4
nabilaa chal <nabilaa.chal@hotmail.com>
2023-11-30 12:34:04 -0800

    a

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,15 @@
-#!/bin/bash                                                                   
-# a Bash script that takes in a URL, sends a GET request to the URL, and displays the body of the response                                                  
-if [ "$(curl -sLI "$1" -X GET | grep "200 OK" | cut -d' ' -f2)" = '200' ]; then curl -sL "$1"; fi
+#!/bin/bash
+
+# Check if a URL is provided as an argument
+if [ -z "$1" ]; then
+    echo "Usage: $0 <URL>"
+    exit 1
+fi
+
+# Use curl to send a GET request and display the body if the status code is 200
+response=$(curl -s -o /dev/null -w "%{http_code}" "$1")
+
+if [ "$response" -eq 200 ]; then
+    body=$(curl -s "$1")
+    echo "$body"
+fi
