090912b2f92268d51ab002718ef52dfd35abd198
nabilaa chal <nabilaa.chal@hotmail.com>
2023-12-01 11:32:05 -0800

    a

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
-"""fetches https://alx-intranet.hbtn.io/status
-"""
+"""Python script that fetches https://intranet.hbtn.io/status."""
 
+from urllib import request
 
 if __name__ == "__main__":
-    import urllib.request
-
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
-        content = res.read()
+    with request.urlopen("https://intranet.hbtn.io/status") as page:
+        content = page.read()
         print("Body response:")
         print("\t- type: {}".format(type(content)))
         print("\t- content: {}".format(content))
