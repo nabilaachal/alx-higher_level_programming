089aebc19b6c2131ffd3a7cf4c22c4b46f5dfe51
nabilaa chal <nabilaa.chal@hotmail.com>
2023-12-01 12:01:15 -0800

    a

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,44 +1,14 @@
 #!/usr/bin/python3
-"""  fetches https://intranet.hbtn.io/status  """
+"""
+Module 0-hbtn-status
+Fetches https://intranet.hbtn.io/status
+"""
+
 
 import urllib.request
-
-url = 
-
-url =
-'https://alx-intranet.hbtn.io/status'
-
-
-
-wit
-with urllib.request.urlopen(url) as response:
-    body = response.read()
-    utf8_content = body.decode(
-    body = response.read()
-    utf8_content = body.
-
-    body = response.read()
-    utf8_con
-
-    body = response.read()
-
-    body = res
-'utf-8')
-
-    
-
-    pr
-print("Body response:")
-    print("    - type:", type(body))
-    
-    pri
-print("    - content:", body)
-    
-  
-print("    - utf8 content:", utf8_content)
-
-`
-chmod +x 0-hbtn_status.py
-./0-hbtn_status.py |
-./0-hbtn_sta
-cat -e
+with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+    html = response.read()
+print('Body response:')
+print("\t- type: {}".format(type(html)))
+print("\t- content: {}".format(html))
+print("\t- utf8 content: {}".format(html.decode('utf-8')))
